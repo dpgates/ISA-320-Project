884c80074d8b4b3a0f2e07bbee5b29097adfa1ca
dpgates <42521764+dpgates@users.noreply.github.com>
2018-10-30 16:35:44 -1000

    Add files via upload
    This is just the beginning files of the program. Feel free to add to the files

---
+++ Project/sql-pt.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3 
+"""
+users table including password 
+accounts table 
+
+
+"""
+import sqlite3
+class Bank():
+    
+class BankUser():
+
+class BankMangt():
+
+class Accnt():
+
+def create_db():
+
+def crt_user():
+
+def crt_mangt():
+
+def accnt():
+
+def main():
+
+conn = sqlite3.connect('banker')
+c = conn.cursor()
+for row in c.execute('SELECT * FORM users '):
+    print(row)
+    print(row[0])
+    print(row[1])
---
+++ Project/users.sql
@@ -0,0 +1,5 @@
+DROP TABLE if exists ;
+CREATE TABLE users (users varchar(10) primary key, password varchar(20));
+INSERT INTO users VALUES("bob", "password");
+
+
