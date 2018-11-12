0469f137c51f6027ddbfac2f8985e20ce208b6ee
dpgates <42521764+dpgates@users.noreply.github.com>
2018-11-12 11:58:58 -1000

    Add files via upload

--- Project/users.sql
+++ Project/users.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS manager;
+DROP TABLE IF EXISTS accout;
 CREATE TABLE users (ID INT PRIMARY KEY  NOT NULL, 
                     NAME           TEXT NOT NULL,
                     PASSWORD       VARCHAR NOT NULL
@@ -7,6 +8,9 @@ CREATE TABLE users (ID INT PRIMARY KEY  NOT NULL,
 CREATE TABLE manager (ID INT PRIMARY KEY  NOT NULL, 
                     NAME           TEXT NOT NULL,
                     PASSWORD       VARCHAR NOT NULL
+                    );
+CREATE TABLE account (ID INT PRIMARY KEY  NOT NULL, 
+                    BALANCE          INT NOT NULL
                     );
 INSERT INTO users VALUES(1, "bob", "password");
 
